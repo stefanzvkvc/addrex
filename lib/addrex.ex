0bd1169b2eb36d20f0a29e3c782c0b0951ad1ca0
Stefan <zivkovicstefan011@gmail.com>
2025-06-12 17:21:05 +0200

    upload project

---
+++ lib/addrex.ex
@@ -0,0 +1,18 @@
+defmodule Addrex do
+  @moduledoc """
+  Documentation for `Addrex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Addrex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/addrex_test.exs
@@ -0,0 +1,8 @@
+defmodule AddrexTest do
+  use ExUnit.Case
+  doctest Addrex
+
+  test "greets the world" do
+    assert Addrex.hello() == :world
+  end
+end
